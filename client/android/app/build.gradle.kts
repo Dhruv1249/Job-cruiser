7b0d7d8023d55f825dee57f26b325131cedee30f
Dhruv1249 <dhr1249.lm@gmail.com>
2026-08-23 23:00:44 +0530

    Configure distinct debug and release application IDs and labels for side-by-side installation

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -29,13 +29,19 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["appName"] = "Job Cruiser"
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            manifestPlaceholders["appName"] = "Job Cruiser (Dev)"
+        }
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            manifestPlaceholders["appName"] = "Job Cruiser"
         }
     }
 }
